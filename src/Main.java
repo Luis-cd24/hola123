069e11309f4396ce23dba0e7e6e3df187aaaba58
Luis <luiscarlos.alvarenga24@gmail.com>
2025-08-04 18:59:49 -0600

    feat: probando con un hola mundo y un commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print("Hello and welcome!");
+
+        System.out.println("Hola mundo, aqui va mi primer commit");
+    }
+}
