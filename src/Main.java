635f0da2d1eede001f2d101b9b27a68fd73ec8af
Luis <luiscarlos.alvarenga24@gmail.com>
2025-08-04 19:04:13 -0600

    feat: ingreso de edad y si es mayor puede pasar.

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Scanner lc = new Scanner(System.in);
+        int edad;
         System.out.print("Hello and welcome!");
 
         System.out.println("Hola mundo, aqui va mi primer commit");
+        System.out.println("Ingrese su edad:");
+        edad=lc.nextInt();
+        if (edad >=18){
+            System.out.println("Usted puede pasar !");
+
+        }else{
+            System.out.println("Acceso denegado.");
+        }
     }
 }
